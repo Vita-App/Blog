616063b5293ed67cfbfde96e42538d8d7eadb10a
Rahul Mishra <robom460@gmail.com>
2022-10-03 15:11:26 +0530

    skeleton

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+import type { HeadFC } from "gatsby"
+
+
+const IndexPage = () => {
+  return (
+    <h1>Vita Blog Skeleton</h1>
+  )
+}
+
+export default IndexPage
+
+export const Head: HeadFC = () => <title>Home</title>
