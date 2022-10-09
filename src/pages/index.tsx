ed4b9d097a15ef0e920a7dc0eb3e9f06a75c514e
Senor-Ducky <robom460@gmail.com>
2022-10-09 13:49:58 +0530

    Graphql-query to strapi API, WIP Article Card and Section, Local Dev Instructions in ReadME

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,46 @@ import "../styles/global.css";
 
 // Component imports
 import NavigationBar from "../components/NavigationBar";
+import ArticleCard from "../components/ArticleCard";
 
 const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allStrapiArticle {
+        edges {
+          node {
+            strapi_id
+            Title
+            Date
+            Author
+            Content {
+              data {
+                Content
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <>
       <NavigationBar />
+      <div className="flex justify-center my-5 align-center">
+        <h1>Articles</h1>
+      </div>
+      <div className="flex justify-center align-center">
+        <div className="grid tablet:grid-cols-3  gap-5 p-4">
+          {data.allStrapiArticle.edges.map(({ node }: any) => (
+            <ArticleCard
+              title={node.Title}
+              date={node.Date}
+              author={node.Author}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
